0b4a3698297e55d11959aac93ac955811e2876ca
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-07-08 16:21:27 -0300

    Resolução do requisito 07

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { shape, string, number } from 'prop-types';
 
 class ProductCard extends Component {
   render() {
     const { product } = this.props;
-    const { title, price, thumbnail } = product;
+    const { id, title, price, thumbnail } = product;
 
     return (
       <li data-testid="product">
         <h2>{ title }</h2>
         <img src={ thumbnail } alt={ title } />
         <p>{ price }</p>
+        <Link to={ `/details/${id}` } data-testid="product-detail-link">Detalhes</Link>
         <button type="button">Adicionar ao carrinho</button>
       </li>
     );
